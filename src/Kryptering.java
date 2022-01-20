c71f870a54da0f6a167b7a4cf229badcbb52cc5b
Demarcily <60388907+Demarcily@users.noreply.github.com>
2022-01-20 13:41:05 +0100

    Array stuff

--- src/Kryptering.java
+++ src/Kryptering.java
@@ -2,14 +2,17 @@ public class Kryptering {
   public static void main (String [] agrs) {
     String message = "HEJ";
     char key = '(';
-    String crypt = "";
-    for (int i = 0 ; i < message.length() ; i++) {
-      System.out.println(Integer.toBinaryString(message.charAt(i)));
+    char[] crypt = message.toCharArray();
+
+    for (int i = 0 ; i < crypt.length ; i++) {
+      System.out.println(Integer.toBinaryString(crypt[i]));
       System.out.println("0" + Integer.toBinaryString(key));
-      System.out.println(Integer.toBinaryString(message.charAt(i)^key) + "\n");
-      crypt += (char)(message.charAt(i)^key);
+      System.out.println(Integer.toBinaryString(crypt[i]^key) + "\n");
+      crypt[i] ^= key;
     }
 
     System.out.println(crypt);
+
+
   }
 }
---
+++ src/KrypteringTemplate.java
@@ -0,0 +1,14 @@
+public class KrypteringTemplate {
+  public static void main (String [] agrs) {
+    String message = "HEJ";
+    char key = '(';
+    String crypt = "";
+    for (int i = 0 ; i < message.length() ; i++) {
+      System.out.println(Integer.toBinaryString(message.charAt(i)));
+      System.out.println("0" + Integer.toBinaryString(key));
+      System.out.println(Integer.toBinaryString(message.charAt(i)^key) + "\n");
+      crypt += (char)(message.charAt(i)^key);
+    }
+    System.out.println(crypt);
+  }
+}
