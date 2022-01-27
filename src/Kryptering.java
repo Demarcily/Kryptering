eb25f83aa417bc1c69cab4b0f9653bd43ab6e52f
Demarcily <60388907+Demarcily@users.noreply.github.com>
2022-01-27 13:30:55 +0100

    Just checking keys

--- src/Kryptering.java
+++ src/Kryptering.java
@@ -12,7 +12,6 @@ public class Kryptering {
     for (int i = 0; i < crypt.length; i++) {
       crypt[i] ^= key[i];
       System.out.print(crypt[i]);
-
     }
 
     System.out.println("\n");
@@ -21,5 +20,12 @@ public class Kryptering {
       crypt[i] ^= key[i];
       System.out.print(crypt[i]);
     }
+
+    System.out.println("Keys for each letter, in order");
+    for (int i = 0; i < key.length; i++) {
+      System.out.println("Key nr " + i + " " + key[i]);
+    }
+
+
   }
 }
