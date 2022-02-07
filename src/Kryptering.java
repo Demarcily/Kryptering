e69aeba5de6a112015d4ef22bf312406eb15e251
Demarcily <60388907+Demarcily@users.noreply.github.com>
2022-02-07 09:34:40 +0100

    Trying to get file reading to work

--- src/Kryptering.java
+++ src/Kryptering.java
@@ -1,5 +1,39 @@
+import java.io.*;
+
 public class Kryptering {
   public static void main (String [] agrs) {
+    String filenameTxt = "Text.txt";
+    int NoN = 10;
+
+    try {
+      PrintWriter txtOut = new PrintWriter(new BufferedOutputStream(new FileOutputStream(filenameTxt)));
+      txtOut.println("Hello Suffering");
+      txtOut.flush();
+      txtOut.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    File f = new File("Text.txt");
+    NoN = (int) f.length();
+
+
+    try {
+      char[] Message = new char[NoN];
+      System.out.println("Text data");
+      BufferedReader txtIn = new BufferedReader(new FileReader(filenameTxt));
+      for (int i = 0 ; i < NoN ; i++) {
+        Message[i] = (char) txtIn.read();
+        System.out.println(Message[i]);
+      }
+      txtIn.close();
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    /// Edit the shit above
+  /*
     String message = "Something is something and I want something";
     char[] LetterKeys = {'n', 'v', ')', 'a', 'm', 'i'};
     char[] key = new char[message.length()];
@@ -21,11 +55,11 @@ public class Kryptering {
       System.out.print(crypt[i]);
     }
 
-    System.out.println("Keys for each letter, in order");
+    System.out.println("\n\nKeys for each letter, in order");
     for (int i = 0; i < key.length; i++) {
       System.out.println("Key nr " + i + " " + key[i]);
     }
 
-
+   */
   }
 }
